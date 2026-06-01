c507d5b5b253a3cda9c79e7aba1f60f33bfd4f9e
viswanadh2000 <47271233+viswanadh2000@users.noreply.github.com>
2026-05-31 23:36:57 -0500

    Use reference board stylesheet only

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import './styles.css';
-import './patch.css';
-import './patch2.css';
+import './board.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
