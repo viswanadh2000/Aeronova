f5e2ba435e30a32126ca6aafcc73848f6ec39a39
viswanadh2000 <47271233+viswanadh2000@users.noreply.github.com>
2026-05-31 22:52:40 -0500

    Import secondary AeroNova CSS patch

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './patch.css';
+import './patch2.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
